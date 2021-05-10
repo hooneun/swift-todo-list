89836b7ae114139092e2051226e8159e9b0058f2
hooneun <kimhoon0316@gmail.com>
2021-05-10 21:32:28 +0900

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by 김훈 on 2021/05/10.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
